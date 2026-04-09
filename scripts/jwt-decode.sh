3ec250a4f2be90922bf2fa9b0de6774d2881150a
Lars Backman <lars@backman.fyi>
2026-04-09 06:47:43 +0200

    chore: local config updates
    - ghostty: bump font size 11 -> 12
    - jwt-decode: also decode header (segment 0) in addition to payload
    - nvim: update lazy-lock.json
    - .githooks: track executable bit

--- scripts/jwt-decode.sh
+++ scripts/jwt-decode.sh
@@ -1,2 +1,3 @@
 #! /bin/bash
-jq -R 'split(".") | .[1] | @base64d | fromjson' <<< "$1"
+jq -R 'split(".") | .[0] | @base64d | fromjson' <<<"$1"
+jq -R 'split(".") | .[1] | @base64d | fromjson' <<<"$1"
